bab104c4ca228195106879e3a981715c4cd34bc4
liz andry <mxlizandry@gmail.com>
2020-04-21 15:28:09 -0700

    oops

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { fetchAllUsers, fetchAllTeams, fetchUser, fetchTeam } from './api-helpers/get-fetchers';
+import { fetchAllUsers, fetchAllTeams } from './api-helpers/get-fetchers';
 class App extends React.Component {
     constructor(props: object) {
         super(props);
